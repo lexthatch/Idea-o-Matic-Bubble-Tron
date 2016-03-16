4c7515154f7b1a10b0a8b10d47af15f61a5f47ed
Scott-Gillatt <scottgillatt@gmail.com>
2016-03-15 20:51:14 -0600

    Size Change working
    Size Change Working.  Animation only on the button picked working.

--- app/components/controllers/controller.js
+++ app/components/controllers/controller.js
@@ -1,21 +1,34 @@
-app.controller('appController', function($scope) {
-    $scope.bubbles = [{ idea: "b", count: 0 }]
+app.controller('appController', function($scope,$timeout) {
+    $scope.bubbles = [{ idea: "b", count: 1 }]
 
+    $scope.generateBubbles = function(){
+        for (var i = 0; i < 20; i++){
+        $scope.bubbles.push(
+            {
+            idea:   "Hello"+i,
+            event: "Somewhere Over The Rainbow",
+            count: 0
+            })
+        }
+    }();
     // $scope.bubbles = [{ idea: "Star Wars Epiosde 8 Ending", count: 0 }, { idea: "Star Wars Epiosde 7 Ending", count: 0 }, { idea: "Star Wars Epiosde 1 Ending", count: 0 }, { idea: "Star Wars Epiosde 2 Ending", count: 0 }, { idea: "Star Wars Epiosde 3 Ending", count: 0 }, { idea: "Star Wars Epiosde 4 Ending", count: 0 }, { idea: "Star Wars Epiosde 5 Ending", count: 0 }, { idea: "Star Wars Epiosde 6 Ending", count: 0 }, { idea: "Star Wars Epiosde 9 Ending", count: 0 }]
 
-    $scope.bumpUp = function(obj) {
+    $scope.bumpUp = function(obj, index) {
         // var temp = document.getElementById('bubbles')
         // var bubbleSize = temp.getBoundingClientRect();
-        return obj.count++
+        // $scope.class = "bubble2"
+        $('#'+ index).animateCss('bounce')
+        obj.count++
+        return ;
     }
-    $scope.bumpDown = function(obj) {
-        // $('#bubble').animateCss('bounceInDown')
+    $scope.bumpDown = function(obj, index) {
+        $('#'+ index).animateCss('shake')
         obj.count--
         return;
     }
-    $scope.pop = function(obj) {
-        // $('#bubble').animateCss('bounceOut')
-        $scope.bubbles.splice($scope.bubbles.indexOf(obj), 1)
+    $scope.pop = function(obj, index) {
+        $('#'+ index).animateCss('fadeOut')
+        $timeout(function(){$scope.bubbles.splice($scope.bubbles.indexOf(obj), 1)},750)
         return;
     }
 
@@ -29,19 +42,16 @@ app.controller('appController', function($scope) {
             $scope.newIdea = "";
         }
         else {
-
+            
             $scope.bubbles.push(
                 {
                     idea: $scope.newIdea.idea,
-                    event: $scope.newIdea.event,
+                    // event: $scope.newIdea.event,
                     count: 0
                 })
-            // $scope.newIdea = "";
-
-            // $('#bubbles').animateCss('bounce')
+                $timeout(function(){$('#' + $scope.newIdea.idea).animateCss('bounce')},100)
         }
     }
-
 
     $.fn.extend({
         animateCss: function(animationName) {
