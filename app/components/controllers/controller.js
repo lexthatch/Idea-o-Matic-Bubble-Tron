30b6a89eae7e9538ad2e0c6c8045e78b07a57418
Scott-Gillatt <scottgillatt@gmail.com>
2016-03-17 15:13:31 -0600

    Set Timeout function to prevent too many button pushes

--- app/components/controllers/controller.js
+++ app/components/controllers/controller.js
@@ -1,24 +1,48 @@
-app.controller('appController', function ($scope) {
-    $scope.bubbles = [{idea: "Star Wars Epiosde 8 Ending", count: 0 }, { idea: "Star Wars Epiosde 7 Ending", count: 0 }, { idea: "Star Wars Epiosde 1 Ending", count: 0 }, { idea: "Star Wars Epiosde 2 Ending", count: 0 }, { idea: "Star Wars Epiosde 3 Ending", count: 0 }, { idea: "Star Wars Epiosde 4 Ending", count: 0 }, { idea: "Star Wars Epiosde 5 Ending", count: 0 }, { idea: "Star Wars Epiosde 6 Ending", count: 0 }, { idea: "Star Wars Epiosde 9 Ending", count: 0 }]
+app.controller('appController', function($scope,$timeout) {
+    $scope.bubbles = []
 
-    $scope.bumpUp = function (obj) {
+    $scope.generateBubbles = function(){
+        for (var i = 0; i < 10; i++){
+        $scope.bubbles.push(
+            {
+            idea: chance.name({middle: true, prefix: true}),
+            event: chance.domain(),
+            // count: 2
+            count: chance.integer({min: 0, max: 4})
+            })
+        }
+    }();
+    // $scope.bubbles = [{ idea: "Star Wars Epiosde 8 Ending", count: 0 }, { idea: "Star Wars Epiosde 7 Ending", count: 0 }, { idea: "Star Wars Epiosde 1 Ending", count: 0 }, { idea: "Star Wars Epiosde 2 Ending", count: 0 }, { idea: "Star Wars Epiosde 3 Ending", count: 0 }, { idea: "Star Wars Epiosde 4 Ending", count: 0 }, { idea: "Star Wars Epiosde 5 Ending", count: 0 }, { idea: "Star Wars Epiosde 6 Ending", count: 0 }, { idea: "Star Wars Epiosde 9 Ending", count: 0 }]
+
+    $scope.bumpUp = function(obj, index) {
+        // var temp = document.getElementById('bubbles')
+        // var bubbleSize = temp.getBoundingClientRect();
+        // $scope.class = "bubble2"
+        $scope.buttonDisabled = true;
         obj.count++
-        
-        // $scope.bubbles.sort($scope.update(""))
-        var temp = $scope.bubbles.indexOf(obj)
-        return console.log(temp);
+        $('#'+ index).animateCss('bounce')
+        if (obj.count >= 5){
+            $timeout(function(){$scope.pop(obj)},10000)
+        }
+        $timeout(function(){$scope.buttonDisabled = false},750);
+        return;
     }
-    $scope.bumpDown = function (obj) {
+    $scope.bumpDown = function(obj, index) {
+        $scope.buttonDisabled = true;
         obj.count--
+        $('#'+ index).animateCss('shake')
+        $timeout(function(){$scope.buttonDisabled = false},750);
         return;
     }
-    $scope.pop = function (obj) {
-        $scope.bubbles.splice($scope.bubbles.indexOf(obj), 1)
+    $scope.pop = function(obj, index) {
+        $scope.buttonDisabled = true;
+        $('#'+ index).animateCss('fadeOut')
+        $timeout(function(){$scope.bubbles.splice($scope.bubbles.indexOf(obj), 1); $scope.buttonDisabled=false},500)
         return;
     }
-    
+
     $scope.newThoughtLength = 25;
-    $scope.newThought = function () {
+    $scope.newThought = function() {
         if (($scope.newIdea.idea === null) || ($scope.newIdea.idea == "")) {
             alert("Sorry, plese enter in a valid input")
         }
@@ -27,27 +51,23 @@ app.controller('appController', function ($scope) {
             $scope.newIdea = "";
         }
         else {
+            
             $scope.bubbles.push(
                 {
                     idea: $scope.newIdea.idea,
-                    event: $scope.newIdea.event,
+                    // event: $scope.newIdea.event,
                     count: 0
                 })
-        }
-    }
-    
-    $scope.update = function (property) {
-        return function (a, b) {
-            if (a[property] < b[property]) {
-                return -1
-            }
-            else if (a[property] > b[property]) {
-                return 1
-            }
-            else {
-                return 0
-            }
+                $timeout(function(){$('#' + $scope.newIdea.idea).animateCss('bounce')},100)
         }
     }
 
+    $.fn.extend({
+        animateCss: function(animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
 });
