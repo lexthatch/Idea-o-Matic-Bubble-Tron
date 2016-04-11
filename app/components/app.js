c5d2db28c6470d3bf32b4e59a95ac3e34516ecf2
Scott-Gillatt <scottgillatt@gmail.com>
2016-04-10 22:55:26 -0600

    Views are now chained

--- app/components/app.js
+++ app/components/app.js
@@ -1,29 +1,33 @@
 /*global angular */
 var app = angular.module("app", ["ngRoute", "ngAnimate"]);
 
-app.config(function ($routeProvider, $locationProvider) {
+app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
-    
-  $routeProvider
-  .when("/", {
-    controller: "appController",
-    templateUrl: "/app/components/routes/main.html"
-  })
-  .when("/view2", {
-    controller: "appController",
-    templateUrl: "/app/components/routes/view2.html"
-  })
-  .when("/view3", {
-    controller: "appController",
-    templateUrl: "/app/components/routes/view3.html"
-  })
-  .when("/view4", {
-    controller: "appController",
-    templateUrl: "/app/components/routes/view4.html"
-  })
-//   .when("/view5", {
-//     controller: "appController",
-//     templateUrl: "/app/components/routes/view2.html/view5.html"
-//   })
-  .otherwise({redirectTo: "/"});
+
+    $routeProvider
+        .when("/", {
+            controller: "appController",
+            templateUrl: "/app/components/routes/main.html"
+        })
+        .when("/view1", {
+            controller: "appController",
+            templateUrl: "/app/components/routes/view1.html"
+        })
+        .when("/view2", {
+            controller: "appController",
+            templateUrl: "/app/components/routes/view2.html"
+        })
+        .when("/view3", {
+            controller: "appController",
+            templateUrl: "/app/components/routes/view3.html"
+        })
+        .when("/view4", {
+            controller: "appController",
+            templateUrl: "/app/components/routes/view4.html"
+        })
+        //   .when("/view5", {
+        //     controller: "appController",
+        //     templateUrl: "/app/components/routes/view2.html/view5.html"
+        //   })
+        .otherwise({ redirectTo: "/" });
 });
