c328d3e6c5e3419ec93a114ea7baed11778cdae6
Scott-Gillatt <scottgillatt@gmail.com>
2016-04-17 23:11:40 -0600

    Fire Base Hooked Up, Semi Working
    Have Fire Base Hooked Up.  Trying to get the count to actually be
    updated on the Database.

--- app/components/app.js
+++ app/components/app.js
@@ -1,5 +1,5 @@
 /*global angular */
-var app = angular.module("app", ["ngRoute", "ngAnimate"]);
+var app = angular.module("app", ["ngRoute", "ngAnimate", "firebase"]);
 
 app.config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
---
+++ app/components/services/firebase.js
@@ -0,0 +1,24 @@
+app.factory('Bubbles', ["$firebaseArray",  
+    function($firebaseArray){
+     var myFireBaseRef = new Firebase("https://bubblesbcw.firebaseio.com/");
+        return $firebaseArray(myFireBaseRef);
+    }  
+]);
+
+
+
+
+
+// angular.module("app").controller("SampleCtrl", function($scope, $firebaseArray) {
+//   var ref = new Firebase("https://bcw2016winter.firebaseio.com/messages");  
+//   $scope.messages = $firebaseArray(ref);
+//   // add new items to the array
+//   // the message is automatically added to our Firebase database!
+//   $scope.addMessage = function() {
+//     $scope.messages.$add({
+//       text: $scope.newMessageText
+//     });
+//     // clear out the "old" message
+//     //$scope.newMessageText = "";
+//   };
+// });
